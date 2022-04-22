643692ecf8422457ac11ce326e73f7edf093fb9f
name <thaonguyen.201.9b@gmail.com>
2022-04-22 07:13:53 +0700

    commit

---
+++ BackEnd/ltddnc_backend/ltddnc_backend/Migrations/TEst.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ltddnc_backend.Migrations
+{
+    public class TEst
+    {
+    }
+}
